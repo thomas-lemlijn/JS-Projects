f6adf4cd9c5e3f08dfe233ca6d7fea961b41044a
Thomas <thomas.lemlijn@gmail.com>
2026-02-17 17:58:23 +0100

    Add reset button functionality with associated styles and icon

--- JS-Counter-Simple/script.js
+++ JS-Counter-Simple/script.js
@@ -2,8 +2,10 @@ var btnMinus = document.getElementById("container-minus");
 var value = document.getElementById("container-value");
 var btnPlus = document.getElementById("container-plus");
 
+var btnReset = document.getElementById("reset");
 
 let numValue = value.innerHTML
+let defaultValue = numValue;
 
 btnMinus.addEventListener("click", function () {
     numValue--;
@@ -11,5 +13,10 @@ btnMinus.addEventListener("click", function () {
 })
 btnPlus.addEventListener("click", function () {
     numValue++;
+    value.innerHTML = numValue;
+})
+
+btnReset.addEventListener("click", function () {
+    numValue = defaultValue;
     value.innerHTML = numValue;
 })
