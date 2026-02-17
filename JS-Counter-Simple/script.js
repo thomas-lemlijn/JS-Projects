ef5abec0b9c86fa29804ac081153e9e8a834f497
Thomas <thomas.lemlijn@gmail.com>
2026-02-17 17:30:09 +0100

    Turn back to original version of counter

--- JS-Counter-Simple/script.js
+++ JS-Counter-Simple/script.js
@@ -1,27 +1,15 @@
-var btnMinus10 = document.getElementById("container-minus-10");
 var btnMinus = document.getElementById("container-minus");
 var value = document.getElementById("container-value");
 var btnPlus = document.getElementById("container-plus");
-var btnPlus10 = document.getElementById("container-plus-10");
 
 
 let numValue = value.innerHTML
 
-btnMinus10.addEventListener("click", function () {
-    numValue -= 10;
-    value.innerHTML = numValue;
-})
 btnMinus.addEventListener("click", function () {
     numValue--;
     value.innerHTML = numValue;
 })
-
 btnPlus.addEventListener("click", function () {
     numValue++;
-    value.innerHTML = numValue;
-})
-btnPlus10.addEventListener("click", function () {
-    numValue++;
-    numValue += 9;
     value.innerHTML = numValue;
 })
